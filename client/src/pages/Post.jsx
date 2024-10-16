69fd4967caa03a3201cb48b09bc931f2a224de58
Abhithesaffron <abhishekmishra02022002@gmail.com>
2024-10-16 14:43:40 +0530

    minor change

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { AuthContext } from "../helpers/AuthContext";
 import { CircularProgress, Box, Typography, Button, Alert, Paper } from "@mui/material";
-import './Post.css'; // Import the CSS file for the Post component
+import './post.css'; // Import the CSS file for the Post component
 
 function Post() {
   const { id } = useParams();
